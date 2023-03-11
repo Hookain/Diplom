cca5ace5f81c1a9edb69e7f2b076c4259095307c
hookain <arny20169@gmail.com>
2023-03-11 20:18:35 +0200

    display values, making comm for on/off button work

--- my_lamps.php
+++ my_lamps.php
@@ -1,3 +1,95 @@
+<?php
+
+include_once('config.php');
+
+/*
+// Retrieve devices from ThingsBoard using the access token
+$options = array(
+    'http' => array(
+        'header' => "X-Authorization: Bearer ".$_SESSION['token']."\r\n",
+        'method' => 'GET'
+    )
+);
+
+$context = stream_context_create($options);
+$result = file_get_contents($devicesUrl, false, $context);
+
+if ($result === false) {
+    echo "Failed to retrieve data from ThingsBoard.";
+    exit;
+}
+
+$data = json_decode($result);
+if ($data === null) {
+    echo "Error parsing response from ThingsBoard.";
+    exit;
+}
+
+
+// Retrieve keys from ThingsBoard using the access token
+$result = file_get_contents($keysUrl, false, $context);
+
+if ($result === false) {
+    echo "Failed to retrieve data from ThingsBoard.";
+    exit;
+}
+
+$keys = json_decode($result);
+if ($keys === null) {
+    echo "Error parsing response from ThingsBoard.";
+    exit;
+}
+
+// Retrieve values from ThingsBoard using the access token
+$result = file_get_contents($valuesUrl, false, $context);
+
+if ($result === false) {
+    echo "Failed to retrieve data from ThingsBoard.";
+    exit;
+}
+
+$values = json_decode($result);
+if ($values === null) {
+    echo "Error parsing response from ThingsBoard.";
+    exit;
+}
+*/
+
+// Retrieve relay status from ThingsBoard using the access token
+
+$postdata = new stdClass();
+$postdata->method = "getStatus";
+$postdata->params = new stdClass();
+
+$postdata = json_encode($postdata);
+
+$options = array(
+    'http' => array(
+        'method'  => "POST",
+        'header'  => "Content-type: application/json\r\n"."X-Authorization: Bearer ".$_SESSION['token']."\r\n",
+        'content' => $postdata,
+        'ignore_errors' => true
+    )
+);
+
+$context = stream_context_create($options);
+$result = file_get_contents($controlUrl, false, $context);
+
+if ($result === false) {
+    echo "Failed to retrieve data from ThingsBoard.";
+    exit;
+}   
+
+$status = json_decode($result);
+if ($status === null) {
+    echo "Error parsing response from ThingsBoard.";
+    exit;
+}
+
+$status = json_decode($status);
+
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -7,8 +99,13 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="./my_lamps.css" />
-  <title>SSL Website</title>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="./my_lamps.css" />  
+
+  <title>SmartLights Website</title>
 
   <style>
 		body {
@@ -47,119 +144,54 @@
       <div class="lamp-info__name">Lamp</div>
       <div class="lamp-info__status">
         <div class="lamp-info__status-text">Status:</div>
-        <button class="lamp-info__toggle on">On</button>
+        <button class="lamp-info__toggle <?=$status->{0} ? 'on' : 'off'?>"><?=$status->{0} ? 'On' : 'Off'?></button>
       </div>
     </div>
   </article>
 </section>
 <section>
   <article>
-    <div>
-      <label for="humidity">H:</label>
-      <input type="text" id="humidity" name="humidity" value="">
-    </div>
-    <div>
-      <label for="temperature">T:</label>
-      <input type="text" id="temperature" name="temperature" value="">
-    </div>
-    <div>
-      <label for="wind">W:</label>
-      <input type="text" id="wind" name="wind" value="">
-    </div>
-  </article>
+      <div>        
+        T: <span><?=$status->t?></span>
+      </div>
+      <div>        
+        H: <span><?=$status->H?></span>
+      </div>
+      <div>        
+        W: <span><?=$status->W?></span>
+      </div>
+
+    </article>
 </section>
 
 <script>
-
-<?php
-
-// Login request to obtain access token
-$login_data = array(
-    'username' => 'viktor.v.apostolov.2018@elsys-bg.org',
-    'password' => 'Viktor'
-);
-
-$options = array(
-    'http' => array(
-        'header' => "Content-type: application/json\r\n",
-        'method' => 'POST',
-        'content' => json_encode($login_data)
-    )
-);
-
-$context = stream_context_create($options);
-$result = file_get_contents('http://zaimov.eu:8181/api/auth/login', false, $context);
-
-if ($result === false) {
-    echo "Failed to log in.";
-    exit;
-}
-
-$token = json_decode($result)->token;
-
-// Retrieve data from ThingsBoard using the access token
-$url = 'http://zaimov.eu:8181/api/tenant/devices?pageSize=100&page=0';
-
-$options = array(
-    'http' => array(
-        'header' => "X-Authorization: Bearer $token\r\n",
-        'method' => 'GET'
-    )
-);
-
-$context = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-
-if ($result === false) {
-    echo "Failed to retrieve data from ThingsBoard.";
-    exit;
-}
-
-$data = json_decode($result);
-if ($data === null) {
-    echo "Error parsing response from ThingsBoard.";
-    exit;
-}
-
-// Display the data on your web server
-echo "<table>";
-echo "<tr><th>ID</th><th>Name</th></tr>";
-
-foreach ($data->data as $device) {
-    $id = $device->id->id;
-    $name = $device->name;
-    echo "<tr><td>$id</td><td>$name</td></tr>";
-}
-
-echo "</table>";
-
-?>
-
-
-
 const toggleBtn = document.querySelector('.lamp-info__toggle');
-let isOn = true;
+let isOn = <?=$status->{0} ? 1 : 0?>;
 toggleBtn.addEventListener('click', function() {
   if (isOn) {
     // Turn off
-    toggleBtn.textContent = 'Off';
-    toggleBtn.classList.remove('on');
-    toggleBtn.classList.add('off');
-    isOn = false;
+    $.post( "control.php", {status: false}, function( data ) {
+      if(data.success){
+        toggleBtn.textContent = 'Off';
+        toggleBtn.classList.remove('on');
+        toggleBtn.classList.add('off');
+        isOn = false;
+      }
+    });    
   } else {
     // Turn on
-    toggleBtn.textContent = 'On';
-    toggleBtn.classList.remove('off');
-    toggleBtn.classList.add('on');
-    isOn = true;
+    $.post( "control.php", {status: true}, function( data ) {
+      if(data.success){
+        toggleBtn.textContent = 'On';
+        toggleBtn.classList.remove('off');
+        toggleBtn.classList.add('on');
+        isOn = true;
+      }
+    });    
   }
 });
 </script>
 
-
-  <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js"></script>
 
 </body>
 </html>
